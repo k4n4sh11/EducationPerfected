654373d0c456a459599030b46b266a2e3d0ac483
k4n4sh11 <130224826+k4n4sh11@users.noreply.github.com>
2023-04-21 14:38:49 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const DIR = {
-        email: 'YOUR EMAIL',  // replace YOUR EMAIL with your email for auto login (keep everything else the same)
-        password: 'YOUR PASSWORD',  // replace YOUR PASSWORD with your password for auto login
+        email: 'bjorg18@eq.edu.au',  // replace YOUR EMAIL with your email for auto login (keep everything else the same)
+        password: 'Jorgolove7**',  // replace YOUR PASSWORD with your password for auto login
 
         login_url: 'https://app.educationperfect.com/app/login',
 
